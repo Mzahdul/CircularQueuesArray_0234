bf9b2612accb8c43a274e3d3afc20b611eccd02f
Mzahdul <seeglas8@gmail.com>
2026-05-26 10:20:42 +0700

    bagian public class

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -11,3 +11,17 @@ private:
     int FRONT, REAR;
     int queue_array[max];
 
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+    }
+};
