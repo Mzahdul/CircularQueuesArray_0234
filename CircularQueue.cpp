aca8bd59d32847580f3ff51a4f008e73cb585954
Mzahdul <seeglas8@gmail.com>
2026-05-26 10:20:28 +0700

    class queue dan private

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// CircularQueuesArray_4NIM
+// 15 commit
+
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
