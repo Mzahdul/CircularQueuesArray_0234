79df67c0950ca4efedea8a93461c3df9210ba111
Mzahdul <seeglas8@gmail.com>
2026-05-26 10:50:00 +0700

    case 4

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -148,6 +148,11 @@ int main() {
                     q.remove();
                     break;
                 }
+                case '3': 
+                {
+                    q.display();
+                    break;
+                }
                 
     }
 
